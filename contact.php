65744c369acf3afe9e9183184938fc9bb2018edb
unknown <b.camara.diaby@outlook.com>
2024-12-13 02:12:17 +0100

    modif

--- contact.php
+++ contact.php
@@ -1,8 +1,9 @@
 <?php
+require 'vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
